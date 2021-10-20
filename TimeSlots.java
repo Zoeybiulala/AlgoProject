ba44a9fbf69c9d4e3da55aa91fa2d6b63a24f012
Xinran Liu <angelaliu20011005@gmail.com>
2021-10-20 13:31:06 -0400

    modified timeslots

--- TimeSlots.java
+++ TimeSlots.java
@@ -1,7 +1,49 @@
 public class TimeSlots {
     private int ID;
-    private Rooms classroom;
-    private Courses classAssigned;
+    private Rooms room;
+    private Courses course;
 
-    
+    public TimeSlots(int i){
+        ID = i;
+        room = null;
+        course = null;
+    }
+
+    public TimeSlots(int i, Rooms r){
+        ID = i;
+        room = r;
+        course = null;
+    }
+
+    public TimeSlots(int i, Courses c){
+        ID = i;
+        room = null;
+        course = c;
+    }
+
+    public TimeSlots(int i, Rooms r, Courses c){
+        ID = i;
+        room = r;
+        course = c;
+    }
+
+    public void setRoom(Rooms r){
+        room = r;
+    }
+
+    public void setCourse(Courses c){
+        course = c;
+    }
+
+    public int getID(){
+        return ID;
+    }
+
+    public Rooms getRoom(){
+        return room;
+    }
+
+    public Courses getCourse(){
+        return course;
+    }
 }
