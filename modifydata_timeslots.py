be30e3547072e71864e869afe5791a678e9973e1
Xinran Liu <37320602+xinranliu1@users.noreply.github.com>
2021-11-10 16:30:41 -0500

    modified modifydata_timeslot

--- modifydata_timeslots.py
+++ modifydata_timeslots.py
@@ -1,5 +1,6 @@
 import os
 import re
+from shutil import copyfile
 
 def modifyFile(rPath, wPath):
     f = open(rPath)
@@ -19,7 +20,8 @@ def modifyFile(rPath, wPath):
             line = f.readline()
             continue
         if re.match('Class Times', line):
-            fw.write(line)
+            string = 'Class Times ' + str(countTS(rPath)) +'\n'
+            fw.write(string)
             isClass = True
             line = f.readline()
             continue
@@ -89,6 +91,41 @@ def modifyFile(rPath, wPath):
     f.close()
     fw.close()
 
+def countTS(rPath):
+    f = open(rPath)
+    line = f.readline()
+    count = 0
+    while line != '':
+        if re.match('Rooms', line):
+            break
+        if re.match('Class Times', line):
+            line = f.readline()
+            continue
+        tmp = line.split()
+        if tmp[5].find('-') == -1:
+            if tmp[5].find('M'):
+                count = count + 1
+            if tmp[5].find('TH') and not tmp[5].find('TTH'):
+                count = count + 1
+            if tmp[5].find('T') and not tmp[5].find('TH'):
+                count = count + 1
+            if tmp[5].find('TTH'):
+                count = count + 2
+            if tmp[5].find('W'):
+                count = count + 1
+            if tmp[5].find('F'):
+                count = count + 1
+        else: #when they have -
+            start = tmp[5].split('-')[0]
+            end = tmp[5].split('-')[1]
+            if re.match(start, 'M') and re.match(end, 'TH'):
+                count = count + 4
+            if re.match(start, 'M') and re.match(end, 'F'):
+                count = count + 5
+        line = f.readline()
+    f.close()
+    return count
+
 readDir = 'bmc_data'
 storeDir = 'bmc_data_timeslot_splitted'
 if not os.path.isdir(storeDir):
@@ -100,4 +137,5 @@ for file in files:
     storePath = os.path.join(storeDir, file)
     if file.startswith('c'):
         modifyFile(readPath, storePath)
-
+    if file.startswith('sf'):
+        copyfile(readPath, storePath)
--- run_main.sh
+++ run_main.sh
@@ -2,22 +2,24 @@
 
 set -o errexit
 
+DIR="bmc_data"
+
 for ((i = 0 ; i < 9 ; i++)); do
     if [ $i == 0 ]
     then
-        java Main.java "bmc_data/c_F200$i.txt" "bmc_data/sf_F200$i.txt" "bmc_data/output_F200$i.txt"
+        java Main.java "$DIR/c_F200$i.txt" "$DIR/sf_F200$i.txt" "$DIR/output_F200$i.txt"
     else
-        java Main.java "bmc_data/c_F200$i.txt" "bmc_data/sf_F200$i.txt" "bmc_data/output_F200$i.txt"
-        java Main.java "bmc_data/c_S200$i.txt" "bmc_data/sf_S200$i.txt" "bmc_data/output_S200$i.txt"
+        java Main.java "$DIR/c_F200$i.txt" "$DIR/sf_F200$i.txt" "$DIR/output_F200$i.txt"
+        java Main.java "$DIR/c_S200$i.txt" "$DIR/sf_S200$i.txt" "$DIR/output_S200$i.txt"
     fi
 done
 
 for ((i = 0 ; i < 5 ; i++)); do
     if [ $i == 4 ]
     then
-        java Main.java "bmc_data/c_S201$i.txt" "bmc_data/sf_S201$i.txt" "bmc_data/output_S201$i.txt"
+        java Main.java "$DIR/c_S201$i.txt" "$DIR/sf_S201$i.txt" "$DIR/output_S201$i.txt"
     else
-        java Main.java "bmc_data/c_F201$i.txt" "bmc_data/sf_F201$i.txt" "bmc_data/output_F201$i.txt"
-        java Main.java "bmc_data/c_S201$i.txt" "bmc_data/sf_S201$i.txt" "bmc_data/output_S201$i.txt"
+        java Main.java "$DIR/c_F201$i.txt" "$DIR/sf_F201$i.txt" "$DIR/output_F201$i.txt"
+        java Main.java "$DIR/c_S201$i.txt" "$DIR/sf_S201$i.txt" "$DIR/output_S201$i.txt"
     fi
 done
